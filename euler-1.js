f9ec828da1c8092b64ae1651929212217a529a2f
Tyler Barber <tylerwbarber@gmail.com>
2015-12-13 11:31:31 -0600

    Euler 1 Complete

---
+++ euler-1.js
@@ -0,0 +1,16 @@
+function isMultiple(divisor, firstDiv, secondDiv) {
+  return (number % firstDiv * secondDiv == 0) ? true : (number % firstDiv == 0) ? true : (number % secondDiv == 0) ? true : false
+}
+
+function findSumOfMultiples(min, max, firstMultiple, secondMultiple) {
+  var sumOfMultiples = 0
+  for (number = min; number < max; number++) {
+    if (isMultiple(number, firstMultiple, secondMultiple)) {
+      sumOfMultiples += number
+    }
+  }
+  return sumOfMultiples
+}
+
+
+console.log(findSumOfMultiples(0, 1000, 3, 5))
